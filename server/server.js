824810dcd508577f7e62c96c5a69a12448296c45
Segun Matthews <segunmatthews@outlook.com>
2017-11-23 13:11:46 +0100

    Add event and center routes to server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import eventRoutes from './routes/event-route';
+import centerRoutes from './routes/center-route';
 
 const app = express();
 const port = process.env.port || 8000;
@@ -9,6 +11,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+eventRoutes(app);
+centerRoutes(app);
 app.get('*', (req, res) => {
   res.status(404).send('404: Not Found');
 });
